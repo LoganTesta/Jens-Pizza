b805079c7a3252e9adc5a54a320d513ca1aa800b
logan <logan.testa@outlook.com>
2020-01-02 14:31:26 -0800

    Add more styling to footer

--- assets/javascript/footer.js
+++ assets/javascript/footer.js
@@ -1,7 +1,9 @@
 
 let footer = document.createElement('footer');
 footer.classList.add("footer");
-footer.innerHTML = "Copyright <span class='footer__year'></span>Jen's Pizza.  All rights reserved.";
+footer.innerHTML = '<div class="footer__copyright">' +
+        'Copyright <span class="footer__year"></span>Jen\'s Pizza.  All rights reserved.' +
+        '</div>';
 document.getElementsByClassName("body-wrapper")[0].appendChild(footer);
 
 let currentDate = new Date();
