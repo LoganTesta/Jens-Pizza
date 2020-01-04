a394b426741d4b39403b2597fd05727ba4685077
logan <logan.testa@outlook.com>
2020-01-04 12:51:58 -0800

    Adjust clickable area of logo

--- assets/javascript/logo.js
+++ assets/javascript/logo.js
@@ -1,7 +1,7 @@
 
 let logoContainer = document.createElement('div');
 logoContainer.innerHTML = '<div class="logo-container">' +      
-        '<a href="/index"><img src="assets/images/logo.png" alt="Jen\'s Pizza logo."/></a>' +
+        '<a href="/index"><img class="logo-container__logo" src="assets/images/logo.png" alt="Jen\'s Pizza logo."/></a>' +
         ' </div> ';
 document.getElementsByClassName("body-wrapper")[0].appendChild(logoContainer);
 
